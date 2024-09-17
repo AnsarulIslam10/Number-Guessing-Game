b5247fa0462cd2d85772d4398cdaa4d65ccf1a7b
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-09-17 13:14:09 +0600

    text color change

--- index.js
+++ index.js
@@ -13,17 +13,21 @@ btn.onclick = function () {
     let guess = inputField.value;
     guess = Number(guess);
     if (isNaN(guess)) {
+        textArea.style.color = 'red'
         textArea.textContent = "Please enter a valid number";
     }
     else if (guess < minNum || guess > maxNum) {
+        textArea.style.color = 'red'
         textArea.textContent = "Please enter a valid number";
     }
     else {
         attempts++;
         if (guess < answer) {
+            textArea.style.color = 'yellow'
             textArea.textContent = "Too Low! Try Again";
         }
         else if (guess > answer) {
+            textArea.style.color = 'yellow'
             textArea.textContent = "Too High! Try Again";
 
         }
